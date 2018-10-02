1bb15a4f1334893ed5ae4884cd968fbf050120c4
Malin Lind <mali1710@student.miun.se>
2018-10-02 11:23:24 +0200

    version 3

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const uglify = require("gulp-uglify");
 const watch = require("gulp-watch");
 const cleanCSS = require("gulp-clean-css");
 const concatCSS = require("gulp-concat-css");
+const imagemin = require('gulp-imagemin');
 
 
 /*Flytta HTML-filer*/
@@ -28,18 +29,30 @@ gulp.task("convertcss", function(){
         .pipe(gulp.dest("pub/css"));
 });
 
+/*Minifiera bilder*/
+gulp.task('convertimg', function(){
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('pub/images'));
+});
+
 
 /*Uppdateringskoll med gulptillägget gulp-watch*/
 gulp.task("watcher", function(){
     watch("src/js/*.js", function(){
         gulp.start("convertjs");
     });
+
     watch("src/*.html", function(){
         gulp.start("html");
     });
     watch("src/css/*.css", function(){
         gulp.start("convertcss");
     });
+watch("src/images/*", function(){
+        gulp.start("convertimg");
+})
+
 });
 
-gulp.task("default", ["html", "convertjs", "convertcss", "watcher"]);
+gulp.task("default", ["html", "convertjs", "convertcss", "convertimg", "watcher"]);
--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,23 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+$(document).ready(function() {
+  // grab the initial top offset of the navigation 
+     var stickyNavTop = $('.nav').offset().top;
+     
+     // our function that decides weather the navigation bar should have "fixed" css position or not.
+     var stickyNav = function(){
+      var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+           
+      // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+      // otherwise change it back to relative
+      if (scrollTop > stickyNavTop) { 
+          $('.nav').addClass('sticky');
+      } else {
+          $('.nav').removeClass('sticky'); 
+      }
+  };
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+  stickyNav();
+  // and run it again every time you scroll
+  $(window).scroll(function() {
+    stickyNav();
+  });
+});
